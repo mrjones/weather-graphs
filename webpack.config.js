a43d1b7a3c4f1737985c5972abfd1b4cdb72f40a
Matt Jones <jonesmr@gmail.com>
2017-01-19 02:58:04 +0000

    minor webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-// npm install ts-loader --save
+// webpack --watch
 module.exports = {
   entry: './clientsrc/d3_legacy.js',
   output: {
     filename: 'nws.js'
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ['', '.ts', '.js']
   },
   module: {
     loaders: [
